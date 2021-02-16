c599a0ff7d1219b5ba4edd49e1e2386ae001c76b
Daniel de Jesus <danieldejesus8745@gmail.com>
2021-02-16 15:58:11 -0300

    adding user entity

--- src/main/java/com/my_todolist/controllers/UserController.java
+++ src/main/java/com/my_todolist/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.my_todolist.controllers;
 
+import com.my_todolist.entities.User;
 import com.my_todolist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class UserController
     public UserController(UserService userService)
     {
         this.userService = userService;
+    }
+
+    @PostMapping("create")
+    public User addUser()
+    {
+        return userService.addUser();
     }
 
     @GetMapping("read")
--- src/main/java/com/my_todolist/services/UserService.java
+++ src/main/java/com/my_todolist/services/UserService.java
@@ -1,10 +1,16 @@
 package com.my_todolist.services;
 
+import com.my_todolist.entities.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService
 {
+    public User addUser()
+    {
+        return new User(1L, "Daniel", "daniel@email.com", "daniel123");
+    }
+
     public String getUser()
     {
         return "Hello form User service";
